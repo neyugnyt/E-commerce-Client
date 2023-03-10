2e90f0c1b788edcc322da94701e7b5a6103ba2af
neyugnyt <sakute2k@gmail.com>
2023-03-10 14:25:28 +0700

    nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      NavBarComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
